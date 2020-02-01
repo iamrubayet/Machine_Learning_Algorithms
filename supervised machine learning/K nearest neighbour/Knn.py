662ee1b5d3947e30d19af633a0e8ab4c0f4c6083
rubayet170746 <rafirubayet75@gmail.com>
2020-02-02 00:08:16 +0600

    added emsemble algorithms and boosting algorithms

---
+++ supervised machine learning/K nearest neighbour/Knn.py
@@ -0,0 +1,5 @@
+from sklearn.neighbors import KDTree
+import numpy as np
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+kdt = KDTree(X, leaf_size=30, metric='euclidean')
+kdt.query(X, k=2, return_distance=False)
